8d1b437693ab01ed91e9dc2d81bbdfca22f08cd6
Nils Pascal Illenseer <ni@vm.ag>
2025-02-14 10:00:52 +0100

    fish: Update asdf configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -131,7 +131,17 @@ fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
 eval (/opt/homebrew/bin/brew shellenv)
 
 # add asdf
-source (brew --prefix asdf)/libexec/asdf.fish
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
 
 # direnv
 eval (direnv hook fish)
