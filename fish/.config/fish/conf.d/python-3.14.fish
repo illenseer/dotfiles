31a674e24e1c1fece02ce9deddc343d817fac870
Nils Pascal Illenseer <ni@vm.ag>
2026-01-06 16:48:56 +0100

    fish: Udpate default python to 3.14

--- fish/.config/fish/conf.d/python-3.14.fish
+++ fish/.config/fish/conf.d/python-3.14.fish
@@ -1,2 +1,4 @@
+fish_add_path -gp /Library/Frameworks/Python.framework/Versions/3.12/bin
+fish_add_path -gp /Library/Frameworks/Python.framework/Versions/3.13/bin /Library/Frameworks/PythonT.framework/Versions/3.13/bin
 # Setting PATH for Python 3.14
-fish_add_path -g "/Library/Frameworks/Python.framework/Versions/3.14/bin"
+fish_add_path -gp /Library/Frameworks/Python.framework/Versions/3.14/bin /Library/Frameworks/PythonT.framework/Versions/3.14/bin
