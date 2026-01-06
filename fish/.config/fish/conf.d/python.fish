cad1bb34f81fda4dffc184f74f978599d3e8187a
Nils Pascal Illenseer <ni@vm.ag>
2026-01-06 18:37:43 +0100

    fish: Rename config file for python

--- fish/.config/fish/conf.d/python.fish
+++ fish/.config/fish/conf.d/python.fish
@@ -1,4 +1,6 @@
+# Setting PATH for Python 3.12
 fish_add_path -gp /Library/Frameworks/Python.framework/Versions/3.12/bin
+# Setting PATH for Python 3.13
 fish_add_path -gp /Library/Frameworks/Python.framework/Versions/3.13/bin /Library/Frameworks/PythonT.framework/Versions/3.13/bin
-# Setting PATH for Python 3.14
+# Setting PATH for Python 3.14 (default)
 fish_add_path -gp /Library/Frameworks/Python.framework/Versions/3.14/bin /Library/Frameworks/PythonT.framework/Versions/3.14/bin
